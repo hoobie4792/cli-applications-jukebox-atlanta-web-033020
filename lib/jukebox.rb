50e9c5f96a89a8cabdd341fd25b84836b9e24c7d
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-25 15:19:06 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,7 +32,7 @@ def play(songs)
   user_input = gets.strip
   
   if user_input.to_i > songs.length or user_input.to_i < 1 or not songs.include? user_input.to_s
-    puts "Invalid input, please try again."
+    puts "Invalid input, please try again"
   end
 end
 
